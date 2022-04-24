95922becc49dd0535c012c3fb6541e2847896ef3
Tharuka Gayashan <tharuka.gayashan.5@gmail.com>
2022-04-24 21:28:05 +0530

    folder structure

---
+++ backend/server.js
@@ -0,0 +1,49 @@
+const express = require("express");
+const cors = require("cors");
+const bp = require("body-parser");
+const mongoose = require("mongoose");
+require("dotenv/config");
+
+//Import routes
+
+
+
+
+
+///////////////////
+
+
+const app = express();
+
+const  PORT = 8000 || process.env.PORT;
+const MONGODB_URL = process.env.MONGODB_URL ||"mongodb+srv://admin:admin@online-movie-reservatio.3tpce.mongodb.net/online-movie-reservation?retryWrites=true&w=majority"
+
+app.use(bp.json());
+app.use(cors());
+
+mongoose.connect(MONGODB_URL,{
+    useNewUrlParser:true
+}).then(()=>{
+    console.log("MonogoDB connected!!");
+}).catch((err)=>{
+    console.error(err);
+});
+
+//Routes
+
+
+
+
+
+
+
+//////////////////////////////
+
+app.get("/",(req,res,next)=>{
+    res.send("<center><h1>Online movie Reservation Backend API</h1></center>");
+    next();
+;})
+
+app.listen(PORT,()=>{
+    console.log(`Server up and running on port ${PORT}`);
+});
